53a14a8c9449049171c6c0c44145a6f16b8bbb48
Komal2006 <komaldeepkr99@gmail.com>
2020-01-11 08:14:23 -0500

    square eg

--- moveSquare_Exercise/ViewController.swift
+++ moveSquare_Exercise/ViewController.swift
@@ -10,11 +10,81 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var square: UIView!
+    var timer:Timer?
+    var goToTop = false
+    var goToBottom = true
+//    var goToLeft = false
+//    var goToRight = false
+    
+    let w = UIScreen.main.bounds.width
+    let h = UIScreen.main.bounds.height
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startTimer()
+      
     }
+    
+    @objc func btnUp() {
+        var xPosition = square.frame.origin.x
+        var yPosition = square.frame.origin.y
+        
+        if goToBottom {
+            yPosition += 20
+        } else if goToTop {
+            yPosition -= 20
+        }
+        
+        if yPosition > h {
+           // xPosition = xCordinate()
+            xPosition = w - 20
+            yPosition = h - 70
+            goToTop = true
+            goToBottom = false
+        } else if yPosition < 0 {
+            //xPosition = xCordinate()
+            xPosition = w - 20
+            yPosition +=  70
+            goToTop = false
+            goToBottom = true
+        }
+        
+//        if goToLeft{
+//            xPosition = w - 20
+//            yPosition = h - 70
+//             goToLeft = true
+//            goToRight = false
+//        } else {
+//            goToLeft = false
+//            goToRight = true
+//        }
 
+        let width = square.frame.size.width
+        let height = square.frame.size.height
+
+        UIView.animate(withDuration: 1.0, animations: {
+            self.square.frame = CGRect(x: xPosition, y: yPosition, width: width, height: height)
+        })
+    }
+    
+//    func xCordinate() -> CGFloat{
+//        let randomInt = Int.random(in: 1..<Int(w - 30))
+//        return CGFloat(randomInt)
+//    }
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.3,
+        target: self,
+        selector: #selector(btnUp),
+        userInfo: nil,
+        repeats: true)
+    }
+    func stopTimer() {
+        if timer != nil {
+            timer?.invalidate()
+            timer = nil
+        }
+    }
 
 }
 
